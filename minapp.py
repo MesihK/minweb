f460de8a3fb47185a58d6c63299fc4ef56457d55
Mesih Kilinc <mesihkilinc@gmail.com>
2022-06-24 12:47:13 -0500

    show two pdb files and alingment

--- minapp.py
+++ minapp.py
@@ -1,6 +1,7 @@
 import flask
 from flask import render_template, session, redirect, url_for, request, flash, send_file
 from flask_bootstrap import Bootstrap5
+import jinja2
 
 import eventlet
 eventlet.monkey_patch()
@@ -11,7 +12,8 @@ app.config['SECRET_KEY'] = 'secret!'
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.jinja2', title='')
+    with open('static/AVX54569.1/AVX54569.1.B3R0R7.100.html','r') as f:
+        return render_template('index.jinja2', title='',aln=f.read())
 
 @app.route('/disclaimer', methods=['GET'])
 def disclaimer():
