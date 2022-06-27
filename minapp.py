1360c0c7d02c3d48d6071477bef95ade2d76f5e5
Mesih Kilinc <mesihkilinc@gmail.com>
2022-06-27 14:16:16 -0500

    implement index page

--- minapp.py
+++ minapp.py
@@ -15,9 +15,15 @@ app.config['SECRET_KEY'] = 'secret!'
 with open('jcvi.prost.db.pkl','rb') as f:
     db = load(f)
 
+summary = []
+for p in db:
+    info = db[p]
+    #pid, jcvi, func, class, essentiality, homolog, tm, seqid
+    summary.append([p,info[0][1],info[0][3],info[0][4],info[0][5],info[1][0],info[1][1],info[1][2]])
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.jinja2', title='Home')
+    return render_template('index.jinja2', title='Home', summary=summary)
 
 @app.route('/disclaimer', methods=['GET'])
 def disclaimer():
@@ -27,7 +33,7 @@ def disclaimer():
 def result():
     pid = request.args.get('p', default = '', type = str)
     if pid == '' or pid not in db:
-        return render_template('index.jinja2',title='%s not found'%pid)
+        return render_template('index.jinja2',title='%s not found'%pid, summary=summary)
     res = db[pid]
     return render_template('result.jinja2',title='Results',pid=pid,res=res)
     
