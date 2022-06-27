cccbc564df6c8eb38d1c09d554e9c77adbdc62a1
Mesih Kilinc <mesihkilinc@gmail.com>
2022-06-27 14:03:19 -0500

    implement result page

--- minapp.py
+++ minapp.py
@@ -2,6 +2,7 @@ import flask
 from flask import render_template, session, redirect, url_for, request, flash, send_file
 from flask_bootstrap import Bootstrap5
 import jinja2
+from pickle import load
 
 import eventlet
 eventlet.monkey_patch()
@@ -10,11 +11,23 @@ app = flask.Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = 'secret!'
 
+
+with open('jcvi.prost.db.pkl','rb') as f:
+    db = load(f)
+
 @app.route('/', methods=['GET'])
 def index():
-    with open('static/AVX54569.1/AVX54569.1.B3R0R7.100.html','r') as f:
-        return render_template('index.jinja2', title='',aln=f.read())
+    return render_template('index.jinja2', title='Home')
 
 @app.route('/disclaimer', methods=['GET'])
 def disclaimer():
     return render_template('disclaimer.jinja2', title='Disclaimer')
+
+@app.route('/result', methods=['GET'])
+def result():
+    pid = request.args.get('p', default = '', type = str)
+    if pid == '' or pid not in db:
+        return render_template('index.jinja2',title='%s not found'%pid)
+    res = db[pid]
+    return render_template('result.jinja2',title='Results',pid=pid,res=res)
+    
